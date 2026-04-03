4b4c90a395348590993f78f5448784615af55b02
Алексей Шашин <alexshashin.2023@gmail.com>
2026-04-03 19:15:41 +0500

    Разработанные вспомогательные классы для сервера. Шашин

---
+++ KeyPass_Shashin/Classes/DatabaseManager.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using KeyPass_Shashin.Models;
+
+namespace KeyPass_Shashin.Classes
+{
+    public class DatabaseManager : DbContext
+    {
+        public DbSet<Storage> Storages { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DatabaseManager() => 
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseMySql(
+                "server=127.0.0.1;port=3307;uid=root;pwd=;database=Storage;",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
---
+++ KeyPass_Shashin/Classes/JwtToken.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using KeyPass_Shashin.Models;
+
+namespace KeyPass_Shashin.Classes
+{
+    public class JwtToken
+    {
+        static byte[] Key = Encoding.UTF8.GetBytes("BLEBLEBLE_______BLUBLUBLU________PAMPAMBARAMBARAM");
+        public static string Generate(User user)
+        {
+            JwtSecurityTokenHandler TokenHandler = new JwtSecurityTokenHandler();
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("UserId", user.Id.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(Key),
+                    SecurityAlgorithms.HmacSha256Signature
+                    )
+            };
+            SecurityToken Token = TokenHandler.CreateToken(tokenDescriptor);
+            return TokenHandler.WriteToken(Token);
+        }
+
+        public static int? GetUserIdFromToken(string token)
+        {
+            try
+            {
+                JwtSecurityTokenHandler TokenHandler = new JwtSecurityTokenHandler();
+                TokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken ValidatedToken);
+                JwtSecurityToken JwtToken = (JwtSecurityToken)ValidatedToken;
+                string UserId = JwtToken.Claims.First(x => x.Type == "UserId").Value;
+                return int.Parse(UserId);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
